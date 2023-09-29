a21f1b9d18ebe366d76703103dd6cd8183fcbef5
Kai Bernhard <17817496+kaibernhard@users.noreply.github.com>
2023-09-29 12:03:27 +0200

    Run ./gradlew spotlessApply

--- build.gradle.kts
+++ build.gradle.kts
@@ -174,9 +174,10 @@ licenseReport {
     // If there's a new dependency with a yet unknown license causing this task to fail
     // the license(s) will be listed in build/reports/dependency-license/dependencies-without-allowed-license.json
     allowedLicensesFile = File("$projectDir/allowed-licenses.json")
-    filters = arrayOf(
-        // With second arg true we get the default transformations:
-        // https://github.com/jk1/Gradle-License-Report/blob/7cf695c38126b63ef9e907345adab84dfa92ea0e/src/main/resources/default-license-normalizer-bundle.json
-        LicenseBundleNormalizer(null, true),
-    )
+    filters =
+        arrayOf(
+            // With second arg true we get the default transformations:
+            // https://github.com/jk1/Gradle-License-Report/blob/7cf695c38126b63ef9e907345adab84dfa92ea0e/src/main/resources/default-license-normalizer-bundle.json
+            LicenseBundleNormalizer(null, true),
+        )
 }
