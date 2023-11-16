95bfceae060ab558a4640c19cfd171107c36b5b6
Kai Bernhard <17817496+kaibernhard@users.noreply.github.com>
2023-11-16 12:28:49 +0100

    chore: relax commitlint check
    Allow conventional commits, but do not enforce it

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,8 +2,8 @@ module.exports = {
   rules: {
     "body-case": [2, "always", "sentence-case"],
     "body-leading-blank": [2, "always"],
-    "header-case": [2, "always", "sentence-case"],
+    "header-case": [1, "always", "sentence-case"],
     "header-full-stop": [2, "never", "."],
-    "header-max-length": [2, "always", 50],
+    "header-max-length": [2, "always", 72],
   },
 };
